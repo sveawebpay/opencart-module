9f946a015e149dcf620cf9c944de868fb4cf75c8
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-03-19 10:11:53 +0100

    lib update

--- src/svea/AdminService/AddOrderRowsRequest.php
+++ src/svea/AdminService/AddOrderRowsRequest.php
@@ -9,13 +9,14 @@ require_once 'AdminServiceRequest.php';
 
 /**
  * Admin Service AddOrderRowsRequest class
- * 
+ *
  * @author Kristian Grossman-Madsen
  */
 class AddOrderRowsRequest extends AdminServiceRequest {
-    
+
     /** @var AddOrderRowBuilder $orderBuilder */
     public $orderBuilder;
+    private $amount;
 
     /**
      * @param addOrderRowsBuilder $orderBuilder
@@ -24,90 +25,64 @@ class AddOrderRowsRequest extends AdminServiceRequest {
         $this->action = "AddOrderRows";
         $this->orderBuilder = $addOrderRowsBuilder;
     }
-
+    
     /**
      * populate and return soap request contents using AdminSoap helper classes to get the correct data format
      * @return \Svea\AdminService\AdminSoap\AddOrderRowsRequest
      * @throws \Svea\ValidationException
-     */    
-    public function prepareRequest() {        
-                   
+     */
+    public function prepareRequest( $resendOrderWithFlippedPriceIncludingVat = false) {
         $this->validateRequest();
-
-        $orderRowNumbers = array();        
-        foreach( $this->orderBuilder->orderRows as $orderRow ) {      
-
-            // handle different ways to spec an orderrow            
-            // inc + ex
-            if( !isset($orderRow->vatPercent) && (isset($orderRow->amountExVat) && isset($orderRow->amountIncVat)) ) {
-                $orderRow->vatPercent = \Svea\WebService\WebServiceRowFormatter::calculateVatPercentFromPriceExVatAndPriceIncVat($orderRow->amountIncVat, $orderRow->amountExVat );
-            }
-            // % + inc
-            elseif( (isset($orderRow->vatPercent) && isset($orderRow->amountIncVat)) && !isset($orderRow->amountExVat) ) {
-                $orderRow->amountExVat = \Svea\WebService\WebServiceRowFormatter::convertIncVatToExVat($orderRow->amountIncVat, $orderRow->vatPercent);
-            }
-            // % + ex, no need to do anything
-                              
-            $orderRows[] = new \SoapVar( 
-                new AdminSoap\OrderRow(
-                    $orderRow->articleNumber, 
-                    $orderRow->name.": ".$orderRow->description,
-                    $orderRow->discountPercent,
-                    $orderRow->quantity, 
-                    $orderRow->amountExVat, 
-                    $orderRow->unit, 
-                    $orderRow->vatPercent
-                ),
-                SOAP_ENC_OBJECT, null, null, 'OrderRow', "http://schemas.datacontract.org/2004/07/DataObjects.Webservice" 
-            );
-        }
         
-        $soapRequest = new AdminSoap\AddOrderRowsRequest( 
-            new AdminSoap\Authentication( 
-                $this->orderBuilder->conf->getUsername( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ), 
-                $this->orderBuilder->conf->getPassword( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ) 
+        $this->priceIncludingVat = $this->determineVatFlag( $this->orderBuilder->orderRows, $resendOrderWithFlippedPriceIncludingVat);        
+        $orderRows = $this->getAdminSoapOrderRowsFromBuilderOrderRowsUsingVatFlag( $this->orderBuilder->orderRows, $this->priceIncludingVat );        
+
+        $soapRequest = new AdminSoap\AddOrderRowsRequest(
+            new AdminSoap\Authentication(
+                $this->orderBuilder->conf->getUsername( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
+                $this->orderBuilder->conf->getPassword( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode )
             ),
-            $this->orderBuilder->conf->getClientNumber( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
-            new \SoapVar($orderRows, SOAP_ENC_OBJECT),            
+            $this->orderBuilder->conf->getClientNumber( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
+            new \SoapVar($orderRows, SOAP_ENC_OBJECT),
             AdminServiceRequest::CamelCaseOrderType( $this->orderBuilder->orderType ),
             $this->orderBuilder->orderId
         );
         return $soapRequest;
     }
-        
+
     public function validate() {
         $errors = array();
         $errors = $this->validateOrderId($errors);
         $errors = $this->validateOrderType($errors);
         $errors = $this->validateCountryCode($errors);
-        $errors = $this->validateRowsToAdd($errors);     
+        $errors = $this->validateRowsToAdd($errors);
         $errors = $this->validateRowsHasPriceAndVatInformation($errors);
         return $errors;
     }
-    
+
     private function validateOrderId($errors) {
-        if (isset($this->orderBuilder->orderId) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderId) == FALSE) {
             $errors[] = array('missing value' => "orderId is required.");
         }
         return $errors;
-    }               
+    }
 
     private function validateOrderType($errors) {
-        if (isset($this->orderBuilder->orderType) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderType) == FALSE) {
             $errors[] = array('missing value' => "orderType is required.");
         }
         return $errors;
-    }            
-    
+    }
+
     private function validateCountryCode($errors) {
-        if (isset($this->orderBuilder->countryCode) == FALSE) {                                                        
+        if (isset($this->orderBuilder->countryCode) == FALSE) {
             $errors[] = array('missing value' => "countryCode is required.");
         }
         return $errors;
-    }    
-    
+    }
+
     private function validateRowsToAdd($errors) {
-        if (isset($this->orderBuilder->orderRows) == FALSE) {                                                        
+        if (isset($this->orderBuilder->orderRows) == FALSE) {
             $errors[] = array('missing value' => "orderRows is required.");
         }
         return $errors;
@@ -115,12 +90,12 @@ class AddOrderRowsRequest extends AdminServiceRequest {
 
     private function validateRowsHasPriceAndVatInformation($errors) {
         if( isset($this->orderBuilder->orderRows) ) {
-            foreach( $this->orderBuilder->orderRows as $orderRow ) {                                                        
-                if( !isset($orderRow->vatPercent) && (!isset($orderRow->amountIncVat) && !isset($orderRow->amountExVat)) ) {            
+            foreach( $this->orderBuilder->orderRows as $orderRow ) {
+                if( !isset($orderRow->vatPercent) && (!isset($orderRow->amountIncVat) && !isset($orderRow->amountExVat)) ) {
                     $errors[] = array('missing order row vat information' => "cannot calculate orderRow vatPercent, need at least two of amountExVat, amountIncVat and vatPercent.");
                 }
             }
         }
         return $errors;
-    }
-}        
+    } 
+}
--- src/svea/AdminService/CreditOrderRowsRequest.php
+++ src/svea/AdminService/CreditOrderRowsRequest.php
@@ -35,36 +35,11 @@ class CreditOrderRowsRequest extends AdminServiceRequest {
      * @return Svea\AdminSoap\CreditOrderRowsRequest
      * @throws Svea\ValidationException
      */
-    public function prepareRequest() {
-                   
-        $this->validateRequest();
-        
-        foreach( $this->orderBuilder->creditOrderRows as $orderRow ) {
-
-            // handle different ways to spec an orderrow            
-            // inc + ex
-            if( !isset($orderRow->vatPercent) && (isset($orderRow->amountExVat) && isset($orderRow->amountIncVat)) ) {
-                $orderRow->vatPercent = \Svea\WebService\WebServiceRowFormatter::calculateVatPercentFromPriceExVatAndPriceIncVat($orderRow->amountIncVat, $orderRow->amountExVat );
-            }
-            // % + inc
-            elseif( (isset($orderRow->vatPercent) && isset($orderRow->amountIncVat)) && !isset($orderRow->amountExVat) ) {
-                $orderRow->amountExVat = \Svea\WebService\WebServiceRowFormatter::convertIncVatToExVat($orderRow->amountIncVat, $orderRow->vatPercent);
-            }
-            // % + ex, no need to do anything
-
-            $this->orderRows[] = new \SoapVar( 
-                new AdminSoap\OrderRow(
-                    $orderRow->articleNumber, 
-                    $orderRow->name.": ".$orderRow->description,
-                    $orderRow->discountPercent,
-                    $orderRow->quantity, 
-                    $orderRow->amountExVat, 
-                    $orderRow->unit, 
-                    $orderRow->vatPercent
-                ),
-                SOAP_ENC_OBJECT, null, null, 'OrderRow', "http://schemas.datacontract.org/2004/07/DataObjects.Webservice" 
-            );
-        }
+    public function prepareRequest( $resendOrderWithFlippedPriceIncludingVat = false) {
+        $this->validateRequest();        
+        $this->priceIncludingVat = $this->determineVatFlag( $this->orderBuilder->creditOrderRows, $resendOrderWithFlippedPriceIncludingVat);
+        $this->orderRows = 
+            $this->getAdminSoapOrderRowsFromBuilderOrderRowsUsingVatFlag( $this->orderBuilder->creditOrderRows, $this->priceIncludingVat );
         
         foreach( $this->orderBuilder->rowsToCredit as $rowToCredit ) {       
             $this->rowNumbers[] = new \SoapVar($rowToCredit, XSD_LONG, null,null, 'long', "http://schemas.microsoft.com/2003/10/Serialization/Arrays");
@@ -72,10 +47,10 @@ class CreditOrderRowsRequest extends AdminServiceRequest {
         
         $soapRequest = new AdminSoap\CreditInvoiceRowsRequest( 
             new AdminSoap\Authentication( 
-                $this->orderBuilder->conf->getUsername( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ), 
-                $this->orderBuilder->conf->getPassword( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ) 
+                $this->orderBuilder->conf->getUsername( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ), 
+                $this->orderBuilder->conf->getPassword( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ) 
             ),
-            $this->orderBuilder->conf->getClientNumber( /*strtoupper*/($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
+            $this->orderBuilder->conf->getClientNumber( ($this->orderBuilder->orderType), $this->orderBuilder->countryCode ),
             $this->orderBuilder->distributionType,
             $this->orderBuilder->invoiceId,
                 
@@ -88,20 +63,28 @@ class CreditOrderRowsRequest extends AdminServiceRequest {
     public function validate() {
         $errors = array();
         $errors = $this->validateInvoiceId($errors);
+        $errors = $this->validateInvoiceDistributionType($errors);
         $errors = $this->validateOrderType($errors);
         $errors = $this->validateCountryCode($errors);
-//        $errors = $this->validateRowsToCredit($errors);     
+        $errors = $this->validateHasRows($errors);     
         $errors = $this->validateCreditOrderRowsHasPriceAndVatInformation($errors);
         return $errors;
     }
     
     private function validateInvoiceId($errors) {
         if (isset($this->orderBuilder->invoiceId) == FALSE) {                                                        
-            $errors[] = array('missing value' => "invoiceId is required.");
+            $errors[] = array('missing value' => "invoiceId is required, use setInvoiceId().");
         }
         return $errors;
     }               
 
+    private function validateInvoiceDistributionType($errors) {
+        if (isset($this->orderBuilder->distributionType) == FALSE) {                                                        
+            $errors[] = array('missing value' => "distributionType is required, use setInvoiceDistributionType().");
+        }
+        return $errors;
+    }     
+    
     private function validateOrderType($errors) {
         if (isset($this->orderBuilder->orderType) == FALSE) {                                                        
             $errors[] = array('missing value' => "orderType is required.");
@@ -111,17 +94,19 @@ class CreditOrderRowsRequest extends AdminServiceRequest {
     
     private function validateCountryCode($errors) {
         if (isset($this->orderBuilder->countryCode) == FALSE) {                                                        
-            $errors[] = array('missing value' => "countryCode is required.");
+            $errors[] = array('missing value' => "countryCode is required, use setCountryCode().");
         }
         return $errors;
     }
 
-//    private function validateRowsToCredit($errors) {
-//        if (isset($this->orderBuilder->rowsToCredit) == FALSE) {                                                        
-//            $errors[] = array('missing value' => "rowsToCredit is required.");
-//        }
-//        return $errors;
-//    }
+    private function validateHasRows($errors) {
+        if( (count($this->orderBuilder->rowsToCredit) == 0) &&
+            (count($this->orderBuilder->creditOrderRows) == 0) )
+        {                                                        
+            $errors[] = array('missing value' => "no rows to credit, use setRow(s)ToCredit() or addCreditOrderRow(s)().");
+        }
+        return $errors;
+    }
 
     private function validateCreditOrderRowsHasPriceAndVatInformation($errors) {
         foreach( $this->orderBuilder->creditOrderRows as $orderRow ) {                                                        
