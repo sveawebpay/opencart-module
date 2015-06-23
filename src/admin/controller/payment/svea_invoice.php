3ac7c3260668b2a3083b5ee7b3c37b0c49249d34
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-06-23 17:28:52 +0200

    bump to version 2.6.19

--- src/admin/controller/payment/svea_invoice.php
+++ src/admin/controller/payment/svea_invoice.php
@@ -2,7 +2,7 @@
 class ControllerPaymentsveainvoice extends Controller {
 
     private $error = array();
-    protected $svea_version = '2.6.18';
+    protected $svea_version = '2.6.19';
 
     public function index() {
         $this->load->language('payment/svea_invoice');
--- src/admin/view/template/total/svea_fee.tpl
+++ src/admin/view/template/total/svea_fee.tpl
@@ -22,7 +22,7 @@
                 <table class="form"><tbody>
                     <tr>
                         <td>Version</td>
-                        <td>2.6.18</td>
+                        <td>2.6.19</td>
                     </tr>
                 </tbody></table>
 
