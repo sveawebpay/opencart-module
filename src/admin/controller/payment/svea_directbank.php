e05030b42e3a5d81960a2ad3529d7b17c92f3620
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-11-09 11:01:42 +0100

    bump to version 3.1.0

--- src/admin/controller/payment/svea_directbank.php
+++ src/admin/controller/payment/svea_directbank.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerPaymentsveadirectbank extends Controller {
 	private $error = array();
-        protected $svea_version = '3.0.16';
+        protected $svea_version = '3.1.0';
 
 	public function index() {
 		$this->load->language('payment/svea_directbank');
--- src/admin/controller/total/svea_fee.php
+++ src/admin/controller/total/svea_fee.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerTotalSveaFee extends Controller {
     private $error = array();
-    protected $svea_version = '3.0.16';
+    protected $svea_version = '3.1.0';
 
     public function index() {
         $this->load->language('total/svea_fee');
