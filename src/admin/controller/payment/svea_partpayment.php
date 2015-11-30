6759d127f3c63dec4d257a1836ec15da4cdc6ac1
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-11-30 15:29:50 +0100

    bumpt to version 3.1.2

--- src/admin/controller/payment/svea_partpayment.php
+++ src/admin/controller/payment/svea_partpayment.php
@@ -1,7 +1,7 @@
 <?php
 class ControllerPaymentsveapartpayment extends Controller {
 	private $error = array();
-        protected $svea_version = '3.1.0';
+        protected $svea_version = '3.1.2';
 
 	public function index() {
 		$this->load->language('payment/svea_partpayment');
