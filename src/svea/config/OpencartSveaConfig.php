eef3e1d93aedab8379f67c7aae4e4136c8b4d004
sveateam <sveateam@svea.com>
2017-09-05 11:11:31 +0200

    release 4.0.20

--- src/svea/config/OpencartSveaConfig.php
+++ src/svea/config/OpencartSveaConfig.php
@@ -12,28 +12,25 @@ $root = realpath(dirname(__FILE__));
  *
  * @author anne-hal
  */
-class OpencartSveaConfig implements \Svea\WebPay\Config\ConfigurationProvider
-{
+class OpencartSveaConfig implements \Svea\WebPay\Config\ConfigurationProvider {
 
     public $config;
     public $payment_method;
 
-    public function __construct($config, $payment_method = NULL)
-    {
+    public function __construct($config, $payment_method = NULL) {
         $this->config = $config;
         $this->payment_method = $payment_method;
     }
 
-    public function getEndPoint($type)
-    {
+    public function getEndPoint($type) {
         $type = strtoupper($type);
-        if ($type == "HOSTED") {
+        if($type == "HOSTED"){
             return Svea\WebPay\Config\ConfigurationService::SWP_PROD_URL;
-        } elseif ($type == "INVOICE" || $type == "PAYMENTPLAN") {
+        }elseif($type == "INVOICE" || $type == "PAYMENTPLAN"){
             return Svea\WebPay\Config\ConfigurationService::SWP_PROD_WS_URL;
-        } elseif ($type == "HOSTED_ADMIN") {
+        }elseif($type == "HOSTED_ADMIN"){
             return Svea\WebPay\Config\ConfigurationService::SWP_PROD_HOSTED_ADMIN_URL;
-        } elseif ($type == "ADMIN") {
+        }elseif ($type == "ADMIN") {
             return Svea\WebPay\Config\ConfigurationService::SWP_PROD_ADMIN_URL;
         } elseif ($type == 'CHECKOUT') {
             return Svea\WebPay\Config\ConfigurationService::CHECKOUT_PROD_BASE_URL;
@@ -44,69 +41,59 @@ class OpencartSveaConfig implements \Svea\WebPay\Config\ConfigurationProvider
         }
     }
 
-    public function getMerchantId($type, $country)
-    {
+    public function getMerchantId($type, $country) {
         $card = $this->config->get('svea_card_merchant_id_prod');
-        if ($card == "") {
+        if($card == ""){
             return $this->config->get('svea_directbank_merchant_id_prod');
-        } else {
+        }else{
             return $card;
         }
     }
 
-    public function getPassword($type, $country)
-    {
+    public function getPassword($type, $country) {
         $country = strtoupper($country);
         $lowertype = strtolower($type);
-        if ($lowertype == "paymentplan") {
+        if($lowertype == "paymentplan"){
             return $this->config->get('svea_partpayment_password_' . $country);
         }
-        return $this->config->get('svea_' . $lowertype . '_password_' . $country);
+        return $this->config->get('svea_'.$lowertype.'_password_' . $country);
     }
 
-    public function getSecret($type, $country)
-    {
+    public function getSecret($type, $country) {
         $secret = $this->config->get('svea_card_sw_prod');
-        if ($secret == "") {
+        if($secret == ""){
             return $this->config->get('svea_directbank_sw_prod');
-        } else {
+        }  else {
             return $secret;
         }
     }
 
-    public function getUsername($type, $country)
-    {
+    public function getUsername($type, $country) {
         $country = strtoupper($country);
         $lowertype = strtolower($type);
-        if ($lowertype == "paymentplan") {
+        if($lowertype == "paymentplan"){
             return $this->config->get('svea_partpayment_username_' . $country);
         }
-        return $this->config->get('svea_' . $lowertype . '_username_' . $country);
+        return $this->config->get('svea_'.$lowertype.'_username_' . $country);
 
     }
 
-    public function getClientNumber($type, $country)
-    {
+    public function getClientNumber($type, $country) {
         $country = strtoupper($country);
         $lowertype = strtolower($type);
-        if ($lowertype == "paymentplan") {
+        if($lowertype == "paymentplan"){
             return $this->config->get('svea_partpayment_clientno_' . $country);
         }
-        return $this->config->get('svea_' . $lowertype . '_clientno_' . $country);
+        return $this->config->get('svea_'.$lowertype.'_clientno_' . $country);
     }
 
-    public function getIntegrationCompany()
-    {
+    public function getIntegrationCompany() {
         return "Svea Ekonomi : Opencart 2 module";
     }
-
-    public function getIntegrationPlatform()
-    {
-        return 'Opencart ' . VERSION;
+    public function getIntegrationPlatform() {
+        return 'Opencart '. VERSION;
     }
-
-    public function getIntegrationVersion()
-    {
+    public function getIntegrationVersion() {
         return $this->config->get($this->payment_method . '_version');
     }
 
