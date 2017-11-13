d130b22edbe95f0ce0657c5fa1b0994eb0c0d000
sveateam <sveateam@svea.com>
2017-11-13 17:24:42 +0100

    release 4.0.33

--- src/catalog/view/theme/default/javascript/svea/sco.js
+++ src/catalog/view/theme/default/javascript/svea/sco.js
@@ -55,6 +55,15 @@ $(document).ready(function () {
           }
 
           shippingEl.html(html);
+            $.ajax({
+                type: 'POST',
+                url: 'index.php?route=svea/shipping/save',
+                data: shippingEl,
+                dataType: 'json',
+                success: function () {
+                    updateCartInformation();
+                }
+            });
           $('#sco-details').show("fast", function () {
             stepOneFormEl.addClass('passed');
             postCodeEl.prop('disabled', true);
