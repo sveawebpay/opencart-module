a64d86c0e166b26acb065d9249b518e38a8f408c
sveateam <sveateam@svea.com>
2017-11-30 17:13:51 +0100

    release 4.1.33

---
+++ src/catalog/language/fi-fi/extension/module/checkout.php
@@ -0,0 +1,99 @@
+<?php
+
+$_['text_normal_checkout']			= '<a href="%s">Paina tästä päästäksesi kassalle!</a>';
+$_['text_checkout_into']			= 'Syötä sähköpostiosoitteesi ja postinumerosi jatkaaksesi Svea Ekonomin Checkoutiin.';
+
+$_['entry_email']					= 'Sähköposti';
+$_['entry_password']				= 'Salasana';
+$_['entry_postcode']				= 'Postinumero';
+$_['entry_new_password']			= 'Syötä uusi salasana';
+
+$_['heading_order']					= 'Tilauksen tiedot';
+$_['heading_shipping']				= 'Toimitus';
+$_['heading_misc']					= 'Muuta';
+$_['heading_cart']					= 'Ostoskori';
+$_['heading_payment']				= 'Maksaminen';
+$_['heading_error']					= 'Palvelussa tapahtui virhe!';
+$_['heading_thank_you']				= 'Kiitos tilauksestasi!';
+$_['heading_receipt']				= 'Kuitti';
+
+$_['tooltip_shipping']				= 'Valitse tilauksellesi toimitustapa';
+
+$_['text_au']						= 'Itävalta';
+$_['text_dk']						= 'Tanska';
+$_['text_de']						= 'Saksa';
+$_['text_fi']						= 'Suomi';
+$_['text_no']						= 'Norja';
+$_['text_se']						= 'Ruotsi';
+
+$_['text_back']						= 'Jatka ostoksia';
+$_['text_continue']					= 'Takaisin kauppaan';
+$_['text_login']					= 'Oletko jo rekisteröitynyt?';
+$_['text_logout']					= 'Hei, olet kirjautunut sisään nimellä <b>%s</b>!';
+$_['text_account']					= 'Haluatko rekisteröityä käyttäjäksi?';
+$_['text_coupon']					= 'Onko sinulla alennuskoodi?';
+$_['text_voucher']					= 'Onko sinulla lahjakortti?';
+$_['text_comment']					= 'Haluatko jättää viestin?';
+$_['text_change_cart']				= 'Haluatko muokata ostokoria?';
+
+$_['text_coupon_code']				= 'Olet lisännyt alennuskoodin:';
+$_['text_voucher_code']				= 'Olet lisännyt lahjakortin:';
+
+//$_['text_normal_checkout']			= 'Haluatko maksaa postiennakolla tai oletko yritysasiakas? <a href="%s">Paina tästä</a> päästäksesi eteenpäin!';
+$_['text_safe_shopping']			= 'Svean avulla voit tehdä ostoksia turvallisesti kaupassa %s';
+$_['text_information']				= 'Svean kautta voit maksaa tilauksesi nopeasti, helposti ja turvallisesti. Voit valita itse maksatko laskulla, kortilla vai verkkomaksulla. Aloita syöttämällä sähköpostiosoitteesi ja postinumerosi jatkaaksesi tilauksen tekemistä.';
+$_['text_create_account']			= 'Voit nopeasti ja helposti luoda itsellesi käyttäjätilin, sinun ei tarvitse tehdä muuta kuin syöttää salasana. Luomalla käyttäjätiln pääset muun muassa tarkastelemaan tilaushistoriaasi, sekä saat mahdolisuuden seurata tilaukesi vaiheita. Saat myös jatkossa uutiskirjeemme, josta löydät parhaat uutiset ja tarjoukset.';
+$_['text_newsletter']				= 'Haluatko tilata uutiskirjeemme?';
+$_['text_thank_you']				= 'Kiitos tilauksestasi! Toivomme että olet tyytyväinen tilaamiisi tuotteisiin. Otathan yhteyttä mikäli sinulla on mitä tahansa kysyttävää tilaukseesi liityen, helpoiten teet sen lähettämällä sähköpostia osoitteeseen <a href="mailto:%s">%s</a>';
+$_['text_pay_now']					= 'Tältä sivulta voit helposti vaihtaa maksutapaa, mikäli haluat maksaa mieluummin kortilla tai erämaksulla.';
+
+$_['text_payment_method']			= 'Svea Checkout';
+$_['text_override_subtotal']		= 'Välisumma';
+$_['text_override_shipping']		= 'Toimitus';
+$_['text_override_tax']				= 'Verot';
+$_['text_override_total']			= 'Yhteensä (sis.alv.)';
+
+$_['text_order_comment']			= 'Varmennus on hyväksytty: %s';
+
+$_['item_voucher']					= 'Lahjakortti';
+$_['item_coupon']					= 'Alennuskoodi';
+$_['item_comment']				    = 'Viesti';
+$_['item_shipping']					= 'Toimitus';
+
+$_['error_unknown']					= '<b>Tapahtui virhe</b><br/>Tämä voi johtua joko teknisestä virheestä tai että tietosi eivät täsmää, tarkistathan että olet syöttänyt sähköpostiosoitteesi ja postinumerosi oikein. Mikäli ongelma jatkuu, otathan yhteyttä asiakaspalveluume.';
+$_['error_internal_message']		= 'Yhteydessä Sveaan tapahtui tekninen virhe. Maksutapahtumaa ei ole tehty. Yritä uudelleen tai ota yhteyttä asiakaspalveluumme.<br/><br/>Message from Svea: <br/><b>%s</b>';
+
+$_['error_shipping']				= 'Virhe: Toimitustapaa ei löydy, ota yhteyttä asiakaspalveluun';
+
+$_['error_attempts']				= 'Olet yrittänyt liian monta kertaa';
+$_['error_approved']				= 'Käyttäjätiliäsi ei ole vielä hyväksytty';
+$_['error_login']					= 'Sisäänkrjatuminen epäonnistui, tarkista tiedot';
+
+$_['error_account_email']			= 'Tarkista sähköpostiosoite';
+$_['error_account_exists']			= 'Sähköpostiosoite on jo rekisteröity';
+$_['error_account_postcode']		= 'Tarkista postinumero';
+$_['error_account_password']		= 'Tarkista salasana, sen tulee olla 4-20 merkkiä pitkä';
+$_['error_account_create']			= 'Käyttäjätiin luominen epäonnistui, ota yhteyttä asiakaspalveluun';
+
+$_['error_unknown_coupon']			= 'Alennuskoodi on virheellinen';
+$_['error_unknown_voucher']			= 'Lahjakortti on virheellinen';
+$_['error_no_coupon']				= 'Ole hyvä ja syötä alennuskoodisi';
+$_['error_no_voucher']				= 'Ole hyvä ja syötä lahjakortti';
+
+$_['error_push_inactive']			= 'Virhe: Svea Checkout ei ole käytössä';
+$_['error_push_no_sco_id']			= 'Virhe: Virheellinen SCO ID';
+$_['error_push_no_oc_id']			= 'Virhe: Virheellinen OC ID';
+$_['error_push_no_oc_order']		= 'Virhe: Tilausta ei löydy (OpenCart)';
+$_['error_push_no_sco_order']		= 'Virhe: Tilausta ei löydy (Svea)';
+$_['error_push_oc_id_match']		= 'Virhe: IDt eivät täsmää';
+$_['error_push_not_complete']		= 'Virhe: Varausta ei voitu suorittaa, perutaan...';
+$_['error_push_already_created']	= 'Virhe: Varaus on jo luotu, perutaan...';
+$_['error_push_update']				= 'Virhe: Varauksen päivittäminen epäonnistui';
+
+$_['text_home']                     = 'Etusivu';
+$_['button_continue']               = 'Jatka';
+$_['button_back']                   = 'Takaisin';
+$_['text_basket']                   = 'Ostoskori';
+$_['text_checkout']                 = 'Kassa';
+$_['text_success']                  = 'Onnistui';
+$_['text_change_postcode']          = 'Vaihda';
