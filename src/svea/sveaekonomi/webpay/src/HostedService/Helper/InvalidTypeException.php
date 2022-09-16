60a647227e371543d59a33c94b1ab430b57f2c1a
KFoobar <david@kfoobar.se>
2022-09-16 19:55:18 +0200

    Remove old libraries

--- src/svea/sveaekonomi/webpay/src/HostedService/Helper/InvalidTypeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Svea\WebPay\HostedService\Helper;
-
-class InvalidTypeException extends \Exception
-{
-    // \Exception referes to class Exception outside our \Svea\ namespace
-}
-
